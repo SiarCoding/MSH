c5845d57f5211a70a5d598d8f8fa66bff45da145
SiarCoding <git@stackblitz.com>
2025-08-05 17:33:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MSH
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/SiarCoding/MSH)
---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './framer/styles.css'
+
+import HeroFramerComponent from './framer/hero'
+import AboutFramerComponent from './framer/about'
+import ProjekteFramerComponent from './framer/projekte'
+import ServicesFramerComponent from './framer/services'
+import CtaFramerComponent from './framer/cta'
+import BlogsFramerComponent from './framer/blogs'
+
+export default function App() {
+  return (
+    <div className='flex flex-col items-center gap-3 bg-[rgb(245,_241, 229)]'>
+      <HeroFramerComponent.Responsive/>
+      <AboutFramerComponent.Responsive/>
+      <ProjekteFramerComponent.Responsive/>
+      <ServicesFramerComponent.Responsive/>
+      <CtaFramerComponent.Responsive/>
+      <BlogsFramerComponent.Responsive/>
+    </div>
+  );
+};

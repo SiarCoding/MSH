3bee3bdce092c50982c072e02a42a6b8e3c50f64
SiarCoding <107007572+SiarCoding@users.noreply.github.com>
2025-08-11 23:17:06 +0200

    Add public folder and center hero and header

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './framer/styles.css'
 
 import HeroFramerComponent from './framer/hero'
+import NavbarFramerComponent from './framer/navbar'
 import AboutFramerComponent from './framer/about'
 import ProjekteFramerComponent from './framer/projekte'
 import ServicesFramerComponent from './framer/services'
@@ -10,7 +11,10 @@ import BlogsFramerComponent from './framer/blogs'
 export default function App() {
   return (
     <div className='flex flex-col w-full bg-[rgb(245,_241, 229)]'>
-      <HeroFramerComponent.Responsive/>
+      <div className='relative w-full'>
+        <HeroFramerComponent.Responsive className='flex flex-col items-center text-center pt-20 pb-32'/>
+        <NavbarFramerComponent.Responsive className='absolute top-0 left-1/2 -translate-x-1/2 w-full flex justify-center'/>
+      </div>
       <div className='flex flex-col items-center gap-3'>
         <AboutFramerComponent.Responsive/>
         <ProjekteFramerComponent.Responsive/>
